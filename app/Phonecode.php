0c9d54acd3991769c4bfef592cf635709823ada5
Andrei Susanu <andrei.susanu@gmail.com>
2020-03-22 22:18:37 +0200

    #4 - Creating PhoneCode model and migration

---
+++ app/Phonecode.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class PhoneCode
+ * @package App
+ *
+ * @property int $id
+ * @property int $code
+ * @property string $phone_number
+ * @property DateTime| $created_at
+ * @property DateTime| $updated_at
+ * @property DateTime|null $deleted_at
+ */
+class PhoneCode extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * @return int
+     */
+    public static function generateCode(): int
+    {
+        return random_int(100000, 999999);
+    }
+}
