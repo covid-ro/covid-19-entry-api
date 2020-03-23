a2a9e2272f1b34b492d2b9dec8be125805afb433
Andrei Susanu <andrei.susanu@gmail.com>
2020-03-23 17:41:03 +0200

    For testing purposes, 666666 is the unique phone code

--- app/PhoneCode.php
+++ app/PhoneCode.php
@@ -35,6 +35,7 @@ class PhoneCode extends Model
      */
     public static function generateCode(): int
     {
-        return random_int(100000, 999999);
+        return 666666;
+        //return random_int(100000, 999999); // TODO: uncomment me!!!
     }
 }
