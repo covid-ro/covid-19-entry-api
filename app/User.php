36e4a7bffa02787f3d652a6af775843b906e9f48
Andrei Susanu <andrei.susanu@gmail.com>
2020-03-23 13:52:43 +0200

    Adding query fields for user

--- app/User.php
+++ app/User.php
@@ -19,13 +19,45 @@ use Laravel\Lumen\Auth\Authorizable;
  * @property string $phone_number
  * @property string $country_code
  * @property string $token
- * @property DateTime| $created_at
- * @property DateTime| $updated_at
+ *
+ * @property string|null $name
+ * @property string|null $surname
+ * @property string|null $email
+ * @property string|null $cnp
+ * @property string|null $document_type
+ * @property string|null $document_series
+ * @property string|null $document_number
+ *
+ * @property string|null $travelling_from_country
+ * @property string|null $travelling_from_city
+ * @property DateTime|null $travelling_from_date
+ * @property DateTime $home_country_return_date
+ *
+ * @property string $question_1_answer
+ * @property string $question_2_answer
+ * @property string $question_3_answer
+ *
+ * @property bool $symptom_fever
+ * @property bool $symptom_swallow
+ * @property bool $symptom_breathing
+ * @property bool $symptom_cough
+ *
+ * @property string|null $vehicle_type
+ * @property string|null $vehicle_registration_no
+ *
+ * @property DateTime|null $created_at
+ * @property DateTime|null $updated_at
  * @property DateTime|null $deleted_at
  */
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable, Authorizable, SoftDeletes;
+
+    const DOCUMENT_TYPE_IDENTITY_CARD = 'identity_card';
+    const DOCUMENT_TYPE_PASSPORT = 'passport';
+
+    const VEHICLE_TYPE_AUTO = 'auto';
+    const VEHICLE_TYPE_AMBULANCE = 'ambulance';
 
     /**
      * The attributes that are mass assignable.
--- database/migrations/2020_03_23_085958_create_users_table.php
+++ database/migrations/2020_03_23_085958_create_users_table.php
@@ -19,8 +19,34 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('phone_number', 32)->index();
-            $table->string('country_code', 2);
+            $table->string('country_code', 2)->nullable();
             $table->string('token', 32)->index();
+
+            $table->string('name', 64)->nullable();
+            $table->string('surname', 64)->nullable();
+            $table->string('email', 255)->nullable();
+            $table->string('cnp', 13)->nullable();
+            $table->string('document_type', 16)->nullable();
+            $table->string('document_series', 16)->nullable();
+            $table->string('document_number', 32)->nullable();
+
+            $table->string('travelling_from_country', 2)->nullable(); // TODO: add foreign id
+            $table->string('travelling_from_city', 32)->nullable();
+            $table->date('travelling_from_date')->nullable();
+            $table->date('home_country_return_date')->nullable();
+
+            $table->string('question_1_answer', 512)->nullable();
+            $table->string('question_2_answer', 512)->nullable();
+            $table->string('question_3_answer', 512)->nullable();
+
+            $table->boolean('symptom_fever')->default(false);
+            $table->boolean('symptom_swallow')->default(false);
+            $table->boolean('symptom_breathing')->default(false);
+            $table->boolean('symptom_cough')->default(false);
+
+            $table->string('vehicle_type', 16)->nullable();
+            $table->string('vehicle_registration_no', 16)->nullable();
+
             $table->timestamps();
             $table->softDeletes();
         });
