cf6a7bdfb18c4cffb21f41973e00270f95fe79c1
Andrei Susanu <andrei.susanu@gmail.com>
2020-03-25 17:03:21 +0200

    Creating codes table and model

---
+++ app/Code.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Code
+ * @package App
+ *
+ * @property int $id
+ * @property string $code
+ * @property DateTime|null $created_at
+ * @property DateTime|null $updated_at
+ */
+class Code extends Model
+{
+
+}
