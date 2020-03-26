f6b17192c7ada35a068fd9008ee36a907ffa47a5
Andrei Susanu <andrei.susanu@gmail.com>
2020-03-26 15:29:54 +0200

    Updating SMS credentials @automation

--- app/Providers/StsSmsServiceProvider.php
+++ app/Providers/StsSmsServiceProvider.php
@@ -30,7 +30,7 @@ class StsSmsServiceProvider extends ServiceProvider
             return new SmsClient(
                 new Client($config),
                 (string)env('SMS_WEBAPI_USERNAME'),
-                (string)base64_decode(env('SMS_WEBAPI_PASSWORD'))
+                (string)env('SMS_WEBAPI_PASSWORD')
             );
         });
     }
