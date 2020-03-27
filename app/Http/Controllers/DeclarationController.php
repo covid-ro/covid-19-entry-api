49db63c94dfc57cdfc9b59f5d768ff71c0b4e050
Andrei Susanu <andrei.susanu@gmail.com>
2020-03-27 07:25:47 +0200

    Removing User condition from GetDeclaration

--- app/Http/Controllers/DeclarationController.php
+++ app/Http/Controllers/DeclarationController.php
@@ -153,22 +153,8 @@ class DeclarationController extends Controller
     {
         $responseData = [];
 
-        /** @var User $user */
-        $user = Auth::user();
-
-        /**
-         * Check if the user was identified
-         */
-        if (empty($user->id)) {
-            return response()->json([
-                'status' => 'error',
-                'reason' => 'Unauthorized'
-            ], 401);
-        }
-
         /** @var Declaration|null $declaration */
         $declaration = Declaration::join('declaration_codes', 'declaration_codes.id', '=', 'declarations.declarationcode_id')
-            ->where('declarations.user_id', $user->id)
             ->where('declaration_codes.code', $declarationCode)
             ->select('declarations.*')
             ->first();
@@ -194,21 +180,8 @@ class DeclarationController extends Controller
     {
         $responseData = [];
 
-        /** @var User $user */
-        $user = Auth::user();
-
-        /**
-         * Check if the user was identified
-         */
-        if (empty($user->id)) {
-            return response()->json([
-                'status' => 'error',
-                'reason' => 'Unauthorized'
-            ], 401);
-        }
-
         /** @var Declaration[] $declarations */
-        $declarations = Declaration::where('user_id', $user->id)->pluck('id')->toArray();
+        $declarations = Declaration::pluck('id')->toArray();
 
         $declarationList = [];
 
