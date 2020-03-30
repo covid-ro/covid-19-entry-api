657b72cf2dacc8f494fe15b86c6ecb8bc1e3ab17
Andrei Susanu <andrei.susanu@gmail.com>
2020-03-30 14:31:53 +0300

    Testing phone format

--- app/Service/Sts/SmsClient.php
+++ app/Service/Sts/SmsClient.php
@@ -148,9 +148,9 @@ class SmsClient
             return str_replace('+40', '0', $phoneNumber);
         }
 
-        if ('+' == substr($phoneNumber, 0, 1)) {
-            return str_replace('+', '00', $phoneNumber);
-        }
+//        if ('+' == substr($phoneNumber, 0, 1)) {
+//            return str_replace('+', '00', $phoneNumber);
+//        }
 
         /**
          * TODO: Ask STS about details (not covered by API documentation)
