253e5f6efc0536f373ba2a0aa212bdeedfb0c25d
Andrei Susanu <andrei.susanu@gmail.com>
2020-05-25 09:08:55 +0300

    Preparing for STS testing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(BorderCheckpointTableSeeder::class);
         $this->call(SymptomTableSeeder::class);
-        $this->call(DevSeeder::class);
+        //$this->call(DevSeeder::class);
     }
 }
